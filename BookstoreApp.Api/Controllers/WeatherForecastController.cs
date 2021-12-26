20fc22ca3a97f51ba1a4ed97217d7c55a3da436c
triomphe ramandalahy <triompherivo@gmail.com>
2021-12-26 21:27:28 +0100

    My First commit of this app from udemy course written with jetbrains rider

---
+++ BookstoreApp.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreApp.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.LogInformation("Made call to the endpoint");
+        try
+        {
+            throw new Exception("This is our customizedException");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e,"Fatal Error Occurred");
+            throw;
+        }
+    }
+}
